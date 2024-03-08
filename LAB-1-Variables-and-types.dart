c215a9b783515151288df736934fc19ea99d3b53
liyu <liyudesta22@gmail.com>
2024-03-08 15:36:56 +0300

    Dart LAB

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,11 @@
+// Exercise 1: Create variables to store your name, age, and favorite color, and print them to the console using string interpolation
+
+void main() {
+  // Define variables
+  String name = "Liyu Desta";
+  int age = 21;
+  String favoriteColor = "orange";
+
+  // Print using string interpolation
+  print("My name is $name, I am $age years old, and my favorite color is $favoriteColor.");
+}
